797cb00a1a5c3790171c9e9b2d7a20931bad4bbf
Neil Harlow <neil@y-designs.com>
2018-08-13 17:06:04 -0700

    added tsc into webpack build process, cleaned up build artifacts and dests

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,24 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-    mode: "development",
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     target: "node",
     node: {
         __dirname: false,
         __filename: false,
     },
-    devtool: "inline-source-map",
+    devtool: process.env.NODE_ENV === 'production' ? undefined : "inline-source-map",
     entry: {
-        main: "./dist/index.js",
-        bin: "./dist/bin.js",
-        dat: "./dist/modules/dat/dat.bin.js",
-        db: "./dist/modules/db/db.bin.js",
-        fs: "./dist/modules/fs/fs.bin.js",
-        p2p: "./dist/modules/p2p/p2p.bin.js",
-        eth: "./dist/modules/eth/eth.bin.js",
+        main: "./src/index.ts",
+        bin: "./src/bin.ts",
+        dat: "./src/modules/dat/dat.bin.ts",
+        db: "./src/modules/db/db.bin.ts",
+        fs: "./src/modules/fs/fs.bin.ts",
+        p2p: "./src/modules/p2p/p2p.bin.ts",
+        eth: "./src/modules/eth/eth.bin.ts",
     },
     output: {
-        path: path.resolve(__dirname, 'bin'),
+        path: path.resolve(__dirname, 'dist'),
         filename: (chunkData) => {
             return chunkData.chunk.name === 'main' || chunkData.chunk.name === 'bin' ? '[name].js': 'modules/[name].bin.js';
         },
@@ -28,12 +28,18 @@ module.exports = {
     },
     resolve: {
         // Add `.ts` and `.tsx` as a resolvable extension.
-        extensions: [".mjs", ".js", ".graphql", ".json"]
+        extensions: [".ts", ".tsx", ".mjs", ".js", ".graphql", ".json"]
     },
     module: {
         rules: [
-            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-            // { test: /\.tsx?$/, loader: "ts-loader" },
+            {
+                test: /bin\.(js|ts)$/,
+                use: ['shebang-loader']
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            },
             // https://github.com/graphql/graphql-js/issues/1272
             {
                 test: /\.mjs$/,
@@ -44,11 +50,7 @@ module.exports = {
                 exclude: /node_modules/,
                 test: /\.graphql$/,
                 use: [{ loader: 'graphql-import-loader' }]
-            },
-            {
-                test: /bin\.js$/,
-                use: ['shebang-loader']
-            }
+            },            
         ]
     },
     plugins: [
