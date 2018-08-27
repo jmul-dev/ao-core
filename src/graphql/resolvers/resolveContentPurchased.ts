0f52159d3f3d8a55dfe3629c7174ec63f6451f17
Ryuhei Yokokawa <ryuhei@y-designs.com>
2018-08-27 13:29:55 -0700

    stupid me, forgot to update states

--- src/graphql/resolvers/resolveContentPurchased.ts
+++ src/graphql/resolvers/resolveContentPurchased.ts
@@ -23,32 +23,32 @@ export default (obj: any, args: IContentRequest_Args, context: IGraphqlResolverC
             let clonedContent = {
                 ...response.data[0],
                 nodeId: response.ethAddress,  // AORouter attaches current user address
-                state: 'DOWNLOADING',
+                state: 'PURCHASING',
             }
-
-            // Note: Note that key watch is 100% up to us, no pre-fixing, or anything on the module side.
-            // TODO: How do we get the target encrypted file's dat address?  That's currently not recorded/passed from contentRequest
-            const p2pWatchKeyRequest: AOP2P_Watch_Key_Data = {
-                key: '/AOSpace/VOD/' + clonedContent.metadataDatKey + '/nodes/' + clonedContent.creatorId + '/' + clonedContent.fileDatKey + '/indexData'
-            }
-            context.router.send('/p2p/watchAndGetKey', p2pWatchKeyRequest).then( (watchKeyResponse:IAORouterMessage)=> {
-                // TODO: Use indexData to find and decrypt the decryption key using your private key
-                // TODO: #2, Might want to sort through the indexData first.
-                const indexData = watchKeyResponse.data
-                if( indexData ) {
-                    //Set State to Purchased
-                    clonedContent.state = 'PURCHASED'
-                    clonedContent.encryptedKey = indexData 
-                    context.router.send('/db/user/content/insert', clonedContent).then((insertResponse: IAORouterMessage) => {
-                        resolve(insertResponse.data)
-                    }).catch(reject) // DB user content insert end
-                } else {
-                    // TODO: We might have to loop this watch/get as sometimes someone else might be purchasing from the same node.
-                    reject( new Error('Did not find any indexData for specified key') )
+            context.router.send('/db/user/content/update', clonedContent).then((purchasingUpdateResponse: IAORouterMessage) => {
+                // Note: Note that key watch is 100% up to us, no pre-fixing, or anything on the module side.
+                // TODO: How do we get the target encrypted file's dat address?  That's currently not recorded/passed from contentRequest
+                const p2pWatchKeyRequest: AOP2P_Watch_Key_Data = {
+                    key: '/AOSpace/VOD/' + clonedContent.metadataDatKey + '/nodes/' + clonedContent.creatorId + '/' + clonedContent.fileDatKey + '/indexData'
                 }
+                context.router.send('/p2p/watchAndGetKey', p2pWatchKeyRequest).then( (watchKeyResponse:IAORouterMessage)=> {
+                    // TODO: Use indexData to find and decrypt the decryption key using your private key
+                    // TODO: #2, Might want to sort through the indexData first.
+                    const indexData = watchKeyResponse.data
+                    if( indexData ) {
+                        //Set State to Purchased
+                        clonedContent.state = 'PURCHASED'
+                        clonedContent.encryptedKey = indexData 
+                        context.router.send('/db/user/content/update', clonedContent).then((purchasedUpdateResponse: IAORouterMessage) => {
+                            resolve(purchasedUpdateResponse.data)
+                        }).catch(reject) // DB user content insert end
+                    } else {
+                        // TODO: We might have to loop this watch/get as sometimes someone else might be purchasing from the same node.
+                        reject( new Error('Did not find any indexData for specified key') )
+                    }
 
-            }).catch(reject) // Watch Key end
-
-        }).catch(reject)// Network content get from DB end
+                }).catch(reject) // Watch Key end
+            }).catch(reject) // update to purchasing.
+        }).catch(reject) // Network content get from DB end
     })
 }
