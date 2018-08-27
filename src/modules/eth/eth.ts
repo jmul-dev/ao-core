ee440d32145c53f142868186f23448352cbfa0d3
Neil Harlow <neil@y-designs.com>
2018-08-27 15:18:24 -0700

    eth module can now listen for transaction status

--- src/modules/eth/eth.ts
+++ src/modules/eth/eth.ts
@@ -1,6 +1,7 @@
 import AORouterInterface, { IAORouterRequest } from "../../router/AORouterInterface";
 import Web3 from 'web3';
 import Debug from 'debug';
+import { resolve } from "url";
 const AOContent = require('ao-contracts/build/contracts/AOContent.json');
 const debug = Debug('ao:eth');
 
@@ -16,6 +17,10 @@ export interface IAOEth_NetworkChange_Data {
 
 export interface IAOEth_ContentIsStaked_Data {
     contentId: string;
+}
+
+export interface IAOEth_TX_Data {
+    transactionHash: string;
 }
 
 /**
@@ -35,7 +40,8 @@ export default class AOEth extends AORouterInterface {
         this.rpcMainnet = args.rpcMainnet
         this.rpcRinkeby = args.rpcRinkeby
         this.router.on('/eth/network/set', this._handleNetworkChange.bind(this))
-        this.router.on('/eth/content/isStaked', this._handleContentIsStaked.bind(this))
+        this.router.on('/eth/content/isStaked', this._handleContentIsStaked.bind(this))        
+        this.router.on('/eth/tx', this._handleTx.bind(this))
         debug(`started`)
     }
 
@@ -80,4 +86,47 @@ export default class AOEth extends AORouterInterface {
         }
     }
 
+    /**
+     * This method will fetch the result of a transaction. If the tx is pending or not found,
+     * it will begin checking tx status on each incoming block.
+     * 
+     * @param request.transactionHash The tx we want to get result for
+     */
+    _handleTx(request: IAORouterRequest) {
+        const requestData: IAOEth_TX_Data = request.data;
+        if ( requestData.transactionHash ) {            
+            this._listenForTransactionStatus(requestData.transactionHash).then(({status}) => {
+                request.respond({status})
+            }).catch(request.reject)
+        } else {
+            request.reject(new Error(`No transaction hash supplied`))
+        }
+    }
+
+    private _listenForTransactionStatus(transactionHash: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            // 1. Check for transaction receipt already existing
+            this.web3.eth.getTransactionReceipt(transactionHash, (error, receipt) => {
+                if ( receipt ) {
+                    resolve({status: receipt.status !== '0x0'})
+                } else {
+                    // 2. Receipt does not exist, begin listening intently
+                    const filter = this.web3.eth.filter('latest')
+                    filter.watch((error, result) => {
+                        this.web3.eth.getTransactionReceipt(transactionHash, (error, receipt) => {
+                            if ( error ) {
+                                reject(error)
+                            } else if ( receipt ) {
+                                filter.stopWatching()
+                                // The TX has been added to the chain, now determine status
+                                resolve({status: receipt.status !== '0x0'})
+                            } else {
+                                // no error and no receipt found on this block, keep listening
+                            }
+                        })
+                    })
+                }
+            })
+        })
+    }
 }
