2e53f86a3935cd5077d5c8229ce8d5747398f36b
Ryuhei Yokokawa <ryuhei@y-designs.com>
2018-09-10 17:17:00 -0700

    made sure the test made use of the separate namespace

--- src/modules/p2p/p2p.spec.ts
+++ src/modules/p2p/p2p.spec.ts
@@ -5,18 +5,25 @@ import EthCrypto from 'eth-crypto'
 import 'mocha'
 import AOContent from '../../models/AOContent';
 
-const personA = EthCrypto.createIdentity()
+interface identity {
+    address: string;
+    privateKey: string;
+    publicKey: string;
+}
+
+const personA:identity = EthCrypto.createIdentity()
 const personADecryptionKey = 'teeeesting123'
-const personB = EthCrypto.createIdentity()
+const personB:identity = EthCrypto.createIdentity()
 
 const contentJson = {"id":"4dafd6582efbbfe913c4202cf926b700b3f5700ccebe1faf10d5f61e1e5ffda8","nodeId":"0x9c7caa71129f534223107e4486ed48afd85de5d6","creatorId":"0x9c7caa71129f534223107e4486ed48afd85de5d6","metadataDatKey":"b7e815da776b9d1610e710bf2e8eca3f8d1972112f62f49997ca3281b73a75ee","contentType":"VOD","isFolder":false,"isMutable":false,"title":"asdfasdf","description":"asd fasdf asdf","stake":12092665,"profit":10,"createdAt":1536254388663,"fileUrl":"4dafd6582efbbfe913c4202cf926b700b3f5700ccebe1faf10d5f61e1e5ffda8/video.mp4","fileDatKey":"4dafd6582efbbfe913c4202cf926b700b3f5700ccebe1faf10d5f61e1e5ffda8","fileName":"video.mp4","fileSize":12092665,"fileChecksum":"066fe55d9f3a744fec738c8fdf8e40bf722b9f48","teaserName":"videoTeaser.mp4","teaserUrl":"b7e815da776b9d1610e710bf2e8eca3f8d1972112f62f49997ca3281b73a75ee/videoTeaser.mp4","featuredImageName":"featuredImage.jpg","featuredImageUrl":"b7e815da776b9d1610e710bf2e8eca3f8d1972112f62f49997ca3281b73a75ee/featuredImage.jpg","metadata":{"duration":"24.824800","resolution":1080,"encoding":"h264"}}
 let content = AOContent.fromObject(contentJson)
 
 describe('AO P2P module', () => {
     const storageLocation = path.resolve(__dirname, '../../../data/p2ptest')
+    const dbNameSpace = "/AOTest/"
     const args:AOP2P_Args =  {
         storageLocation: storageLocation,
-        dbNameSpace: "/AOTest/"
+        dbNameSpace: dbNameSpace
     }
     let aoP2P; //scoped outside
     before((done) => {
@@ -105,8 +112,15 @@ describe('AO P2P module', () => {
 
     it('personB watches to see if personA has dropped his keys', () => {
         //As person B
+        const watchKey = AOP2P.routeNodeRegistration({
+            nameSpace: dbNameSpace,
+            contentType: contentJson.contentType,
+            metaDatKey: contentJson.metadataDatKey,
+            ethAddress: personA.address,
+            fileDatKey: content.fileDatKey
+        })
         const watchKeyData: AOP2P_Watch_AND_Get_IndexData_Data = {
-            key: '/AOSpace/VOD/'+contentJson.metadataDatKey + '/nodes/'+ personA.address +'/' + content.fileDatKey + '/indexData',
+            key: watchKey,
             ethAddress: personB.address
         }
         return new Promise((resolve,reject) => {
