12f6398bf8b9b10ee37e291d8c1402f7c4220b49
Ryuhei Yokokawa <ryuhei@y-designs.com>
2018-09-10 17:58:49 -0700

    added fuse.js, modified types, added query argument.  put the results through fuse

--- src/graphql/resolvers/resolveVideos.ts
+++ src/graphql/resolvers/resolveVideos.ts
@@ -2,9 +2,16 @@ import { IGraphqlResolverContext } from '../../http';
 import { generateMockVideoList } from '../mockVideos';
 import { IAORouterMessage } from '../../router/AORouter';
 import { AODB_NetworkContentGet_Data } from '../../modules/db/db';
+import Fuse from 'fuse.js'
 
-export default (obj: any, args: any, context: IGraphqlResolverContext, info: any) => {
+interface IVideos_Args {
+    query?: string;
+}
+
+export default (obj: any, args: IVideos_Args, context: IGraphqlResolverContext, info: any) => {
     return new Promise((resolve, reject) => {
+        //TODO: Add query validation?
+        const {query} = args
         const networkQueryData:AODB_NetworkContentGet_Data = {
             query: {
                 content: {
@@ -17,7 +24,26 @@ export default (obj: any, args: any, context: IGraphqlResolverContext, info: any
             let returnData = networkContentResponse.data.map( (a) => {
                 return a.content
             })
-            resolve(returnData)
+            if(!query) {
+                resolve(returnData)
+            } else {
+                //Fuzzy search
+                const fuseOptions = {
+                    shouldSort: true,
+                    threshold: 0.6,
+                    location: 0,
+                    distance: 100,
+                    maxPatternLength: 32,
+                    minMatchCharLength: 1,
+                    keys: [
+                        "title",
+                        "description"
+                    ]
+                }
+                const fuse = new Fuse(returnData, fuseOptions)
+                let results = fuse.search(query)
+                resolve(results)
+            }
         }).catch(reject)
     })
 }
