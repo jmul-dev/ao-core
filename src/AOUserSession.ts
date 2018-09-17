d2b125ed52af8fb491a367ef02796d1e5c74bd47
Ryuhei Yokokawa <ryuhei@y-designs.com>
2018-09-17 16:51:36 -0700

    fixes for upload content issues

--- src/AOUserSession.ts
+++ src/AOUserSession.ts
@@ -692,6 +692,7 @@ export default class AOUserSession {
      * @param {AOContent} content 
      */
     private _handleContentStaked(content: AOContent) {
+        console.log('doing it! fucking doing it!')
         // 1. Import all the freaken files.
         const fileImportDatData:AODat_ImportSingle_Data = {
             key: content.fileDatKey
--- src/modules/dat/dat.ts
+++ src/modules/dat/dat.ts
@@ -314,27 +314,25 @@ export default class AODat extends AORouterInterface {
                     request.reject(err)
                     return;
                 }
-                try {
-                    dat.importFiles() //Note, this doesn't do a lot for our code base since the import has to be run post file creation.
-                } catch (e) {
-                    debug(e)
-                    request.reject(e)
-                    return
-                }
-                const datKey = dat.key.toString('hex')
-                debug('Created new dat file: dat://' + datKey)
-                this.dats[datKey] = dat;
-                const newDatEntry: DatEntry = {
-                    key: datKey,
-                    complete: true, // assume its complete since already on disk
-                    updatedAt: new Date(),
-                    createdAt: new Date(),
-                }
-                this._updateDatEntry(newDatEntry)
-                request.respond({
-                    ...newDatEntry,
-                    dir: requestData.newDatDir
+                
+                //Note, this doesn't do a lot for our code base since the import has to be run post file creation.
+                dat.importFiles(() => {
+                    const datKey = dat.key.toString('hex')
+                    debug('Created new dat file: dat://' + datKey)
+                    this.dats[datKey] = dat;
+                    const newDatEntry: DatEntry = {
+                        key: datKey,
+                        complete: true, // assume its complete since already on disk
+                        updatedAt: new Date(),
+                        createdAt: new Date(),
+                    }
+                    this._updateDatEntry(newDatEntry)
+                    request.respond({
+                        ...newDatEntry,
+                        dir: requestData.newDatDir
+                    })
                 })
+                    
             })
         } catch (error) {
             debug(`Caught error while attempting to create dat: ${error.message}, path: ${fullPath}`)
