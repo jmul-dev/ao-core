db2efadab9eb7785bcf681f58d09bc6a69e00ce2
Neil Harlow <neil@y-designs.com>
2018-09-18 16:23:37 -0700

    added check during contentRequest to avoid duplicate content ingestion

--- src/graphql/resolvers/resolveContentRequest.ts
+++ src/graphql/resolvers/resolveContentRequest.ts
@@ -1,6 +1,6 @@
 import { IGraphqlResolverContext } from '../../http';
 import AOContent, { AOContentState } from '../../models/AOContent';
-import { AODB_NetworkContentGet_Data } from '../../modules/db/db';
+import { AODB_NetworkContentGet_Data, AODB_UserContentGet_Data } from '../../modules/db/db';
 import { IAORouterMessage } from "../../router/AORouter";
 
 
@@ -10,25 +10,39 @@ interface IContentRequest_Args {
 
 export default (obj: any, args: IContentRequest_Args, context: IGraphqlResolverContext, info: any) => {
     return new Promise((resolve, reject) => {
-        // 1. Pull the Content from network content db
-        let networkContentQuery: AODB_NetworkContentGet_Data = {
-            query: { _id: args.metadataDatKey },
-            contentOnly: true,
+        // 1. Make sure user has not already pulled this content into node
+        let userContentQuery: AODB_UserContentGet_Data = {
+            query: { id: args.metadataDatKey }
         }
-        context.router.send('/db/network/content/get', networkContentQuery).then((response: IAORouterMessage) => {
-            if (!response.data || response.data.length !== 1) {
-                reject(new Error(`No discovered content with id: ${args.metadataDatKey}`))
-                return;
+        context.router.send('/db/user/content/get', userContentQuery).then((response: IAORouterMessage) => {
+            if ( response.data && response.data.length > 0 ) {
+                // User already has this content! Pass through to processContent to try and bump through
+                // the process.
+                const existingContent: AOContent = AOContent.fromObject(response.data[0])
+                resolve(existingContent)
+                context.userSession.processContent(existingContent)
+                return null;
             }
-            // 2. Insert the content into user db (they have begun the content purchase/hosting process)
-            let updatedContent: AOContent = AOContent.fromObject({
-                ...response.data[0],
-                nodeId: context.userSession.id,
-                state: AOContentState.HOST_DISCOVERY,
-            })
-            context.router.send('/db/user/content/insert', updatedContent.toRawJson()).then((insertResponse: IAORouterMessage) => {
-                resolve(updatedContent)
-                context.userSession.processContent(updatedContent)
+            // 2. Pull the Content from network content db
+            let networkContentQuery: AODB_NetworkContentGet_Data = {
+                query: { _id: args.metadataDatKey },
+                contentOnly: true,
+            }
+            context.router.send('/db/network/content/get', networkContentQuery).then((response: IAORouterMessage) => {
+                if (!response.data || response.data.length !== 1) {
+                    reject(new Error(`No discovered content with id: ${args.metadataDatKey}`))
+                    return;
+                }
+                // 3. Insert the content into user db (they have begun the content purchase/hosting process)
+                let updatedContent: AOContent = AOContent.fromObject({
+                    ...response.data[0],
+                    nodeId: context.userSession.id,
+                    state: AOContentState.HOST_DISCOVERY,
+                })
+                context.router.send('/db/user/content/insert', updatedContent.toRawJson()).then((insertResponse: IAORouterMessage) => {
+                    resolve(updatedContent)
+                    context.userSession.processContent(updatedContent)
+                }).catch(reject)
             }).catch(reject)
         }).catch(reject)
     })
