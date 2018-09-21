b681c21548686330e15ca86db67d862ad1f671ea
Ryuhei Yokokawa <ryuhei@y-designs.com>
2018-09-21 11:22:49 -0700

    went further with yargs.  next level

--- src/bin.ts
+++ src/bin.ts
@@ -1,15 +1,46 @@
 #!/usr/local/bin/node
 'use strict';
 import Core, { ICoreOptions } from './index';
+import fsExtra from 'fs-extra'
 
 const argv = require('yargs')
+    .option('disableHttpInterface', {
+        alias: 'd',
+        description: 'Disables the HTTP interface',
+        type: 'boolean'
+    })
+    .option('corePort', {
+        alias: 'port',
+        description: 'Port that Core runs on',
+        type: 'number'
+    })
+    .option('coreOrigin', {
+        description: 'Domain that Core runs on',
+        type: 'string',
+        //Add coerce to validate as URL.
+    })
+    .option('httpOrigin', {
+        description:'CORS allowed origin',
+        type: 'string'
+    })
+    .option('storageLocation', {
+        alias: 's',
+        description: 'Where your data be stored',
+        type: 'string',
+        coerce: (arg) => {
+            //If you use coerce, the default options don't get passed as it goes through this.
+            return fsExtra.pathExistsSync(arg) ? arg : Core.DEFAULT_OPTIONS.storageLocation
+        }
+    })
+    .option('nodeBin', {
+        description: 'Node binary to use',
+        type: 'string',
+        coerce: (arg) => {
+            return fsExtra.pathExistsSync(arg) ? arg : Core.DEFAULT_OPTIONS.nodeBin
+        }
+    })
     .default(Core.DEFAULT_OPTIONS)
-    .describe('disableHttpInterface', 'Disables the HTTP interface')
-    .describe('corePort', 'Port that Core runs on')
-    .describe('coreOrigin', 'Domain that Core runs on')
-    .describe('httpOrigin', 'CORS allowed origin')
-    .describe('storageLocation', 'Where your data be stored')
-    .describe('nodeBin', 'Node binary to use')
     .argv
+
 
 const core = new Core(argv);
