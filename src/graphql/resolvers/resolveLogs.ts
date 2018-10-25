9793444749d0e1047f16edabb237c85c5d53fd93
Neil Harlow <neil@y-designs.com>
2018-10-25 13:11:26 -0700

    closes #114

--- src/graphql/resolvers/resolveLogs.ts
+++ src/graphql/resolvers/resolveLogs.ts
@@ -3,7 +3,11 @@ import { IGraphqlResolverContext } from '../../http';
 export default (obj: any, args: any, context: IGraphqlResolverContext, info: any) => {
     return new Promise((resolve, reject) => {
         context.router.send('/db/logs/get', {ignoreLogging: true}).then(response => {
-            resolve(response.data || [])
+            let logs = response.data || []
+            logs.sort((a, b) => {
+                return new Date(parseInt(b.createdAt)).getTime() - new Date(parseInt(a.createdAt)).getTime();
+            })
+            resolve(logs)
         }).catch(reject)
     })
 }
