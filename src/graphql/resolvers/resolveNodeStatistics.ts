59eeb142ca901eb681c44941d04af799fd396fe2
Neil Harlow <neil@y-designs.com>
2018-11-07 13:35:31 -0800

    logging updates

--- src/graphql/resolvers/resolveNodeStatistics.ts
+++ src/graphql/resolvers/resolveNodeStatistics.ts
@@ -8,8 +8,8 @@ const packageJson = require('../../../package.json');
 
 export default (obj: AOContent, args: any, context: IGraphqlResolverContext, info: any) => {
     return new Promise((resolve, reject) => {
-        const p2pStatsPromise = context.router.send('/p2p/stats')
-        const ethStatsPromise = context.router.send('/eth/stats')
+        const p2pStatsPromise = context.router.send('/p2p/stats', null, {ignoreLogging: true})
+        const ethStatsPromise = context.router.send('/eth/stats', null, {ignoreLogging: true})
         Promise.all([p2pStatsPromise, ethStatsPromise]).then((results: Array<IAORouterMessage>) => {
             const p2pStats: AOP2P_PeerStats = results[0].data
             const ethStats: IAOETH_Stats = results[1].data
@@ -22,7 +22,7 @@ export default (obj: AOContent, args: any, context: IGraphqlResolverContext, inf
                 ethNetworkStatus: ethStats.connectionStatus,
                 ethNetworkId: ethStats.networkId || context.options.networkId,
                 totalContentHosts: ethStats.totalContentHosts,
-            })  
+            })
         }).catch(reject)
     })
 }
