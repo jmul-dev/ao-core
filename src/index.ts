00aa1f7843d4f7514a66ba0262cf842156794794
Neil Harlow <neil@y-designs.com>
2019-01-07 14:15:22 -0800

    start of initialization process rewrite, added state machine to handle startup process logic/sequence

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
-'use strict';
-import { EVENT_LOG, NETWORK_CHANGE, DATA, DATA_TYPES } from './constants';
-import AORouter from './router/AORouter';
-import { IAORouterRequest } from './router/AORouterInterface';
-import Http, { IGraphqlResolverContext } from './http'
-import { EventEmitter } from 'events';
-import path from 'path';
-import AOUserSession from './AOUserSession';
-import exportDataResolver, { IContentExport_Args } from './graphql/resolvers/resolveExportData'
-import importDataResolver, { IContentImport_Args } from './graphql/resolvers/resolveImportData';
-import registerResolver, { IRegister_Args } from './graphql/resolvers/resolveRegister';
-import fsExtra from 'fs-extra'
-import Debug, { debugLogFile } from './AODebug'
+"use strict";
+import { EVENT_LOG, NETWORK_CHANGE, DATA, DATA_TYPES } from "./constants";
+import AORouter from "./router/AORouter";
+import { IAORouterRequest } from "./router/AORouterInterface";
+import Http, { IGraphqlResolverContext } from "./http";
+import { EventEmitter } from "events";
+import path from "path";
+import AOUserSession from "./AOUserSession";
+import exportDataResolver, {
+    IContentExport_Args
+} from "./graphql/resolvers/resolveExportData";
+import importDataResolver, {
+    IContentImport_Args
+} from "./graphql/resolvers/resolveImportData";
+import registerResolver, {
+    IRegister_Args
+} from "./graphql/resolvers/resolveRegister";
+import fsExtra from "fs-extra";
+import Debug, { debugLogFile } from "./AODebug";
 
-const debug = Debug('ao:core');
-const error = Debug('ao:core:error');
-
+const debugLog = Debug("ao:core");
+const errorLog = Debug("ao:core:error");
 
 export interface ICoreOptions {
     ethAddress: string;
@@ -37,96 +42,183 @@ export interface AOCore_NetworkIdMismatch_Data {
     newNetworkId: string;
 }
 
+export const AOCoreState = Object.freeze({
+    INITIAL_STATE: "INITIAL_STATE",
+    LOGS_INTIALIZING: "LOGS_INTIALIZING",
+    LOGS_INTIALIZED: "LOGS_INTIALIZED",
+    ROUTER_INITIALIZING: "ROUTER_INITIALIZING",
+    ROUTER_INITIALIZED: "ROUTER_INITIALIZED",
+    HTTP_INITIALIZING: "HTTP_INITIALIZING",
+    HTTP_INITIALIZED: "HTTP_INITIALIZED",
+    SESSION_INITIALIZED: "SESSION_INITIALIZED",
+    INITIALIZATION_FAILED: "INITIALIZATION_FAILED",
+    SHUTDOWN_ERROR: "SHUTDOWN_ERROR",
+    STARTED: "STARTED"
+});
+
 export default class Core extends EventEmitter {
-    public static DEFAULT_OPTIONS = {
-        ethAddress: '',
-        networkId: '1',
-        disableHttpInterface: false,
-        corePort: 3003,
-        coreOrigin: 'http://localhost',
-        httpOrigin: 'http://localhost:3000',
-        storageLocation: path.resolve(__dirname, '..', 'data'),
-        desktopLocation: undefined,
-        nodeBin: process.execPath,
-        exportData: '', // Takes a path for where the data is exported to
-        importData: '', // Takes a path to the zip file
-    }
     public options: ICoreOptions;
     private coreRouter: AORouter;
     private http: Http;
     private userSession: AOUserSession;
+    private state: String;
+    public static DEFAULT_OPTIONS = {
+        ethAddress: "",
+        networkId: "1",
+        disableHttpInterface: false,
+        corePort: 3003,
+        coreOrigin: "http://localhost",
+        httpOrigin: "http://localhost:3000",
+        storageLocation: path.resolve(__dirname, "..", "data"),
+        desktopLocation: undefined,
+        nodeBin: process.execPath,
+        exportData: "", // Takes a path for where the data is exported to
+        importData: "" // Takes a path to the zip file
+    };
 
     constructor(args: ICoreOptions = Core.DEFAULT_OPTIONS) {
-        super()
-        this.options = Object.assign({}, Core.DEFAULT_OPTIONS, args)
-        debug(this.options)
-        // Overwriting previous debug logs before starting
-        if(process.platform === "win32") {
+        super();
+        this.options = Object.assign({}, Core.DEFAULT_OPTIONS, args);
+        debugLog(`Starting core with options:`);
+        debugLog(this.options);
+        process.stdin.resume(); // Hack to keep the core processes running
+        process.on("exit", () => {
+            if (this.coreRouter) {
+                this.coreRouter.shutdown(); //Ensure that all child processes are killed
+            }
+            debugLog("Core process exiting...");
+        });
+        process.on("SIGINT", () => {
+            process.exit();
+        });
+        this.state = AOCoreState.INITIAL_STATE;
+        this.stateChangeHandler(AOCoreState.INITIAL_STATE);
+    }
+
+    /**
+     * The core initialization process is handled through this state machine. Each
+     * part of the init process will trigger an additional call to `stateChangeHandler`.
+     *
+     * @param nextState
+     * @param errorMessage
+     */
+    private stateChangeHandler(nextState, errorMessage?) {
+        this.state = nextState;
+        switch (nextState) {
+            case AOCoreState.INITIAL_STATE:
+                this.logsInitializer();
+                break;
+            case AOCoreState.LOGS_INTIALIZED:
+                // Logs have been initialized, now we start the core router
+                this.coreRouterInitializer();
+                break;
+            case AOCoreState.ROUTER_INITIALIZED:
+                this.sessionInitializer();
+                break;
+            case AOCoreState.SESSION_INITIALIZED:
+                if (!this.options.disableHttpInterface) {
+                    this.httpInitializer();
+                } else {
+                    this.stateChangeHandler(AOCoreState.STARTED);
+                }
+                break;
+            case AOCoreState.HTTP_INITIALIZED:
+                this.stateChangeHandler(AOCoreState.STARTED);
+                break;
+            case AOCoreState.STARTED:
+                this.postInitializationWork();
+                break;
+            case AOCoreState.INITIALIZATION_FAILED:
+            case AOCoreState.SHUTDOWN_ERROR:
+                // TODO: shutdown?
+                this.handleShutdown(errorMessage);
+                break;
+            default:
+                break;
+        }
+        // TODO: emit state change (pass back up to electron/parent process)
+    }
+
+    private logsInitializer() {
+        this.state = AOCoreState.LOGS_INTIALIZING;
+        if (process.platform === "win32") {
             //Windows has a hard time dealing with filesystem stuff, can't delete something we're writing into.
-            this._init(args)
+            this.stateChangeHandler(AOCoreState.LOGS_INTIALIZED);
         } else {
-            fsExtra.remove(path.join(this.options.storageLocation, debugLogFile)).then(() => {
-                this._init(args)
-            }).catch(console.log)
+            // Remove previous log file
+            const logFilePath = path.join(
+                this.options.storageLocation,
+                debugLogFile
+            );
+            fsExtra
+                .remove(logFilePath)
+                .then(() => {
+                    this.stateChangeHandler(AOCoreState.LOGS_INTIALIZED);
+                })
+                .catch(error => {
+                    errorLog(
+                        `Error removing previous log file at: ${logFilePath}.`,
+                        error
+                    );
+                    this.stateChangeHandler(
+                        AOCoreState.INITIALIZATION_FAILED,
+                        `Unable to locate log file`
+                    );
+                });
         }
     }
-    private _init(args) {
-        this.coreRouter = new AORouter(this.options)
-        this.coreRouter.init().then(() => {
-            this.coreRouter.router.on('/core/log', this._handleLog.bind(this))
-            this.coreRouter.router.on('/core/networkIdMismatch', this._handleNetworkIdMismatch.bind(this))
-            this.userSession = new AOUserSession(this.coreRouter.router)
-            this.http = new Http(this.coreRouter.router, this.options, this.userSession)
-            //Used to handle things like data exports and other command line only options
-            this._handleCommandline(args)
-        }).catch(debug)
-        process.stdin.resume();  // Hack to keep the core processes running
-        process.on('exit', () => {
-            this.coreRouter.shutdown()//Ensure that all child processes are killed
-            debug('Core process exiting...')
-        })
-        process.on('SIGINT', () => {
-            process.exit()
-        })
-    }
 
-    _handleLog(request: IAORouterRequest) {
-        const data: AOCore_Log_Data = request.data
-        if (process.send) {
-            // If there is a parent process (running within app) we relay
-            // all of the logs up.
-            process.send({ event: EVENT_LOG, message: data.message });
-        } else {
-            // TODO: append to a temp log somewhere (make this configurable via command line)
-        }
-        this.coreRouter.router.send('/db/logs/insert', {
-            message: data.message,
-            createdAt: Date.now()
-        }).then(request.respond).catch(request.reject)
-        this.emit('log', { message: data.message })
-    }
-
-    _handleNetworkIdMismatch(request: IAORouterRequest) {
-        const { newNetworkId } = request.data
-        if (process.send) {
-            process.send( {
-                event: NETWORK_CHANGE,
-                newNetworkId: newNetworkId
+    private coreRouterInitializer() {
+        this.coreRouter = new AORouter(this.options);
+        this.coreRouter
+            .init()
+            .then(() => {
+                // Core router event handlers
+                this.coreRouter.router.on(
+                    "/core/log",
+                    this._handleLog.bind(this)
+                );
+                this.coreRouter.router.on(
+                    "/core/networkIdMismatch",
+                    this._handleNetworkIdMismatch.bind(this)
+                );
+                this.stateChangeHandler(AOCoreState.ROUTER_INITIALIZED);
             })
-        } else {
-            //Self kill if network id change happens
-            this.shutdownWithError(new Error('networkId changed'))
-        }
+            .catch(error => {
+                errorLog(`Error initializing core router:`, error);
+                this.stateChangeHandler(
+                    AOCoreState.INITIALIZATION_FAILED,
+                    `Unable to start core router`
+                );
+            });
     }
 
-    _handleCommandline(args: ICoreOptions) {
-        const { exportData, importData, ethAddress, networkId } = args
+    private sessionInitializer() {
+        this.userSession = new AOUserSession(this.coreRouter.router);
+        this.stateChangeHandler(AOCoreState.SESSION_INITIALIZED);
+    }
+
+    private httpInitializer() {
+        this.http = new Http(
+            this.coreRouter.router,
+            this.options,
+            this.userSession
+        );
+        this.stateChangeHandler(AOCoreState.HTTP_INITIALIZED);
+    }
+
+    private postInitializationWork() {
+        this.processCommandLineArgs(this.options);
+    }
+
+    private processCommandLineArgs(args: ICoreOptions) {
+        const { exportData, importData, ethAddress, networkId } = args;
         const context: IGraphqlResolverContext = {
             router: this.coreRouter.router,
             options: this.options,
             userSession: this.userSession
-        }
-        const empty: object = {} //Need an empty object?
+        };
+        const empty: object = {}; //Need an empty object?
 
         if (ethAddress.length && networkId) {
             const registerArgs: IRegister_Args = {
@@ -134,24 +226,32 @@ export default class Core extends EventEmitter {
                     ethAddress: ethAddress,
                     networkId: networkId
                 }
-            }
-            registerResolver(empty, registerArgs, context, empty).then(() => {
-                debug(`ethAddress set as ${ethAddress} and networkId is ${networkId}`)
-            }).catch((error) => {
-                error(error)
-            })
+            };
+            registerResolver(empty, registerArgs, context, empty)
+                .then(() => {
+                    debugLog(
+                        `ethAddress set as ${ethAddress} and networkId is ${networkId}`
+                    );
+                })
+                .catch(error => {
+                    errorLog(error);
+                });
         }
 
         //Exports data
         if (exportData.length) {
             const exportArgs: IContentExport_Args = {
                 inputs: { exportPath: exportData }
-            }
-            exportDataResolver(empty, exportArgs, context, empty).then(() => {
-                debug('Export finished. Export should be at: ' + exportData)
-            }).catch((error) => {
-                error('Bad news, export failed: ', error)
-            })
+            };
+            exportDataResolver(empty, exportArgs, context, empty)
+                .then(() => {
+                    debugLog(
+                        "Export finished. Export should be at: " + exportData
+                    );
+                })
+                .catch(error => {
+                    errorLog("Bad news, export failed: ", error);
+                });
         }
 
         //Imports data
@@ -161,21 +261,61 @@ export default class Core extends EventEmitter {
                     importPath: importData,
                     commandLine: true
                 }
-            }
-            importDataResolver(empty, importArgs, context, empty).then(() => {
-                debug('Import finished. Please restart AO!')
-                this.coreRouter.shutdown()
-                process.exit(0);
-            }).catch((error) => {
-                error('Really bad news, import failed: ', error)
-            })
+            };
+            importDataResolver(empty, importArgs, context, empty)
+                .then(() => {
+                    debugLog("Import finished. Please restart AO!");
+                    this.coreRouter.shutdown();
+                    process.exit(0);
+                })
+                .catch(error => {
+                    errorLog("Really bad news, import failed: ", error);
+                });
         }
     }
 
-    shutdownWithError(err) {
-        error('core shutting down with error\n', err);
-        this.coreRouter.shutdown()
-        process.exit(1);
+    private handleShutdown(errorMessage?) {
+        this.coreRouter.shutdown();
+        if (errorMessage) {
+            errorLog("core shutting down with error\n", errorMessage);
+            process.exit(1);
+        } else {
+            debugLog(`Core shutting down...`);
+        }
     }
 
+    _handleLog(request: IAORouterRequest) {
+        const data: AOCore_Log_Data = request.data;
+        if (process.send) {
+            // If there is a parent process (running within app) we relay
+            // all of the logs up.
+            process.send({ event: EVENT_LOG, message: data.message });
+        } else {
+            // TODO: append to a temp log somewhere (make this configurable via command line)
+        }
+        this.coreRouter.router
+            .send("/db/logs/insert", {
+                message: data.message,
+                createdAt: Date.now()
+            })
+            .then(request.respond)
+            .catch(request.reject);
+        this.emit("log", { message: data.message });
+    }
+
+    _handleNetworkIdMismatch(request: IAORouterRequest) {
+        const { newNetworkId } = request.data;
+        if (process.send) {
+            process.send({
+                event: NETWORK_CHANGE,
+                newNetworkId: newNetworkId
+            });
+        } else {
+            //Self kill if network id change happens
+            this.stateChangeHandler(
+                AOCoreState.SHUTDOWN_ERROR,
+                `Core shutting down due to Ethereum network id mismatch. This is likely a result of switching Ethereum networks.`
+            );
+        }
+    }
 }
