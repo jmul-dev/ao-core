8d7d64b56f3506739a5acc5476684929d942ae27
Neil Harlow <neil@y-designs.com>
2019-03-08 14:52:11 -0800

    fixed url resolver so that it only returns if the corresponding resource is defined

--- src/graphql/resolvers/resolveUrl.ts
+++ src/graphql/resolvers/resolveUrl.ts
@@ -1,16 +1,33 @@
-import Http, { IGraphqlResolverContext } from '../../http';
+import Http, { IGraphqlResolverContext } from "../../http";
 
-
-export default (obj: any, args: any, context: IGraphqlResolverContext, info: any) => {
+export default (
+    obj: any,
+    args: any,
+    context: IGraphqlResolverContext,
+    info: any
+) => {
+    if (!obj[info.fieldName]) {
+        return null;
+    }
     switch (info.fieldName) {
-        case 'teaserUrl':
-            return `${Http.RESOURCES_ENDPOINT}/${obj.metadataDatKey}/${obj.teaserUrl}`
-        case 'featuredImageUrl':
-            return `${Http.RESOURCES_ENDPOINT}/${obj.metadataDatKey}/${obj.featuredImageUrl}`
-        case 'fileUrl':
-            return `${Http.ENCRYPTED_RESOURCES_ENDPOINT}/${obj.fileDatKey}/${obj.fileUrl}`
+        case "teaserUrl":
+            return `${Http.RESOURCES_ENDPOINT}/${obj.metadataDatKey}/${
+                obj.teaserUrl
+            }`;
+        case "featuredImageUrl":
+            return `${Http.RESOURCES_ENDPOINT}/${obj.metadataDatKey}/${
+                obj.featuredImageUrl
+            }`;
+        case "fileUrl":
+            return `${Http.ENCRYPTED_RESOURCES_ENDPOINT}/${obj.fileDatKey}/${
+                obj.fileUrl
+            }`;
         default:
-            console.warn(`Attempting to resolve field[${info.fieldName}] but unkown url resolver`)
-            return obj[info.fieldName]
-    }    
-}
+            console.warn(
+                `Attempting to resolve field[${
+                    info.fieldName
+                }] but unkown url resolver`
+            );
+            return obj[info.fieldName];
+    }
+};
