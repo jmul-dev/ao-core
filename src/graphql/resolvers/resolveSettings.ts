d2f8bb204630a0cc3e06ca1a4d44fcb691f071da
Neil Harlow <neil@y-designs.com>
2019-03-26 16:11:11 -0700

    fixed missing settings field

--- src/graphql/resolvers/resolveSettings.ts
+++ src/graphql/resolvers/resolveSettings.ts
@@ -1,9 +1,29 @@
-import { IGraphqlResolverContext } from '../../http';
+import { IGraphqlResolverContext } from "../../http";
+import { IAORouterMessage } from "../../router/AORouter";
 
-export default (obj: any, args: any, context: IGraphqlResolverContext, info: any) => {
+export default (
+    obj: any,
+    args: any,
+    context: IGraphqlResolverContext,
+    info: any
+) => {
     return new Promise((resolve, reject) => {
-        context.router.send('/db/settings/get', {ignoreLogging: true}).then(response => {
-            resolve(response.data)
-        }).catch(reject)
-    })
-}
+        context.router
+            .send("/db/settings/get", { ignoreLogging: true })
+            .then((settingsResponse: IAORouterMessage) => {
+                context.router
+                    .send("/eth/network/get")
+                    .then((ethNetworkResponse: IAORouterMessage) => {
+                        // fetching rpc endpoint from eth network in case it is not defined/overwritten by settings
+                        resolve({
+                            ...settingsResponse.data,
+                            ethNetorkRpc:
+                                settingsResponse.data.ethNetorkRpc ||
+                                ethNetworkResponse.data.rpcEndpoint
+                        });
+                    })
+                    .catch(reject);
+            })
+            .catch(reject);
+    });
+};
