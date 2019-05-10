fff1a988dae25a6dd06d563a82f0b71cc24c6d89
Neil Harlow <neil@y-designs.com>
2019-05-10 12:26:18 -0700

    failed purchase now rolls back to HOST_DISCOVERY

--- src/AOUserSession.ts
+++ src/AOUserSession.ts
@@ -846,9 +846,13 @@ export default class AOUserSession {
                     };
                 } else {
                     // Transaction failed :(, go back to previous state
+                    // TODO: should go back to DOWNLOADED state if we want
+                    // the ability to resubmit purchase tx for same host.
+                    // For now we assume something bad happened and go back
+                    // to host discovery
                     contentUpdateAfterTx.update = {
                         $set: {
-                            state: AOContentState.DOWNLOADED,
+                            state: AOContentState.HOST_DISCOVERY,
                             "transactions.purchaseTx": null
                         }
                     };
